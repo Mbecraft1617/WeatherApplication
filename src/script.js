152346c747638d85648a7826e66f724bc3fef1e9
Monica Becraft <mbecraft1216@gmail.com>
2022-06-09 21:36:43 -0400

    added and updated basic forecast

--- src/script.js
+++ src/script.js
@@ -37,6 +37,33 @@ let month = months[now.getMonth()];
 
 p.innerHTML = `${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
 
+//forecast
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class = "row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt="weather icon"
+                width="42"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max"> 18° </span>
+                <span class="weather-forecast-temperature-min"> 12° </span>
+              </div>
+            </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //search engine
 
 function showTemperature(response) {
@@ -99,6 +126,8 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+displayForecast();
 
 //current location button
 
